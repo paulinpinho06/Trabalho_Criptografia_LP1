172067f393c7152af2b36fad8872ead421465029
cruzmatheus123 <almeidamatheuscruz65@gmail.com>
2025-11-14 22:45:56 -0300

    Update main.c
    iniciei a integraçao do codigo com a entrada e saida do texto a ser criptografado, talvez possua algum erro.

--- Sources/main.c
+++ Sources/main.c
@@ -6,9 +6,14 @@
 #include "../Headers/menu.h"
 
 int main()
-{
+{   
+    int mat_criada = 0;
+    int texto_pronto = 0;
+    int texto_cifrado = 0;
     int mat[2][2],opcao,det;
     float matInv[2][2],matAdj[2][2];
+    char texto_original[max_texto] = ""; // Texto de entrada
+    char texto_crip[max_texto] = ""; // Texto de saída
     char str_menu[] =    "Digite a opcao desejada\n"
                          "1. Criar uma matriz de criptografia.\n"
                          "2. Digitar um texto para ser criptografado.\n"
@@ -22,12 +27,21 @@ int main()
         opcao = obter_opcao(5);
         switch(opcao){
             case 1:
+                printf("\n--- 1. Criar Matriz de Criptografia ---\n");
                 gerarMat(mat);
                 det = calcDet(mat);
                 calcMatAdj(mat, matAdj);
-                calcMatInv(matAdj, matInv, det);    
+                calcMatInv(matAdj, matInv, det);
+                MATRIZ_CRIADA = 1;
+                printf("Matriz de criptografia e inversa criadas com sucesso!\n");
             break;
             case 2:
+                printf("\n--- 2. Criar Texto Original ---\n");
+                printf("Digite o texto (max %d caracteres): ", max_texto - 1);
+                scanf("%s", texto_original);
+                texto_pronto = 1;
+                texto_cifrado = 0;
+                printf("Texto original definido: %s\n", texto_original);
 
             break;
             case 3:
