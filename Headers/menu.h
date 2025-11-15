36fea4cfa77b060ea242d801594c5237afbac5f3
cruzmatheus123 <almeidamatheuscruz65@gmail.com>
2025-11-15 18:20:07 -0300

    Add max_texto definition and new function prototype
    adicionei ao arquivo o limite de caracteres do texto, alem disso criei o void para que nao ocorra overflow no main.c

--- Headers/menu.h
+++ Headers/menu.h
@@ -1,7 +1,8 @@
 #ifndef MENU_H
 #define MENU_H
-
+#define max_texto 100000
 void mostra_menu(char str_menu[]);
 int obter_opcao(int n_opcoes);
+void ler_e_pre_processar_texto(char *texto_saida, int max_tamanho);
 
 #endif // MENU_H
